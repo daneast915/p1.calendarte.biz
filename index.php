3cacd21ca898ae34ec6b003536473d3faf0bf32f
Daniel East <daneast915@gmail.com>
2013-09-16 19:48:42 -0400

    added ii.php and css, includes and js directories

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
-Welcome to P1
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Daniel East Bio: Summary</title>
+	<link rel="stylesheet" href="css/p1.css" type="text/css" />
+	<link rel="stylesheet" href="css/blueprint/screen.css" type="text/css" media="screen, projection" />
+	<link rel="stylesheet" href="css/blueprint/print.css" type="text/css" media="print" />
+	<!--[if lt IE 8]><link rel="stylesheet" href="css/blueprint/ie.css" type="text/css" media="screen, projection" /><![endif]-->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"> </script>
+	<script type="text/javascript" src="js/iamhere.js"></script>
+</head>
+<body id="index">
+<div class="container">
+  <?php include("includes/header.php"); ?>
+  <?php include("includes/navigation.php"); ?>
+  <div id="main" class="span-20 last">
+    <h2>Who am I?</h2>
+    <p>
+		My name is Dan East. I live in Lancaster, PA with my wife and 5 cats.  I'm a Windows desktop software engineer and work in Exton, PA.
+		We develop the MicroStation product and I specifically work on the core user interface.
+		I have been working as a software engineer since 1985.  I am taking classes at Harvard Extension School to broaden my horizons
+		into the area Web Design and Development.
+	</p>
+    <h2>Hobbies</h2>
+    <p>
+		I am a singer and a runner.  I have sung with numerous choirs, including the 
+		Philadelphia Singers Chorale, 
+		Princeton Singers,
+		Dallas Symphony and
+		Wheatland Chorale.
+		I have run 5 marathons and also did the 2006 Lake Placid Ironman.
+	</p>
+    </div>
+  <?php include("includes/footer.php"); ?>
+</div>
+</body>
+</html>
